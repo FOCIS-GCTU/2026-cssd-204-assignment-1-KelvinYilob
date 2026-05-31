ce6cfe8ff1be33bed4e3e0b87bf4558cddc74c1d
KelvinYilob <2425403575@live.gctu.edu.gh>
2026-05-31 16:47:48 +0000

    initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,34 @@
+# File: initials.py
+# Description: Print out my initials(KNY) in  large block letters.
+# Assignment Number: 1
+#
+# Name:Kelvin Ngmenkoma Yilob
+# STUDENT ID:  2425403575
+# Email: 2425403575@live.gctu.edu.gh
+# Grader: Mr.Augustus Buckman
+#
+# On my honor, Kelvin Ngmenkoma Yilob, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print initials KNY in large block letters, 12x10 each,
+    # with period blocks, and three-period borders, totaling 60 characters wide.
 
+    print()
+    print("...KNY")
+    print()
+    print("...KK......KK........NN......NN.........YY......YY..........")
+    print("...KK.....KK.........NNN.....NN.........YY......YY..........")
+    print("...KK....KK..........NNNN....NN..........YY....YY...........")
+    print("...KK...KK...........NN.NN...NN...........YY..YY............")
+    print("...KKKKKK............NN..NN..NN............YYYY.............")
+    print("...KK...KK...........NN...NN.NN.............YY..............")
+    print("...KK....KK..........NN....NNNN.............YY..............")
+    print("...KK.....KK.........NN.....NNN.............YY..............")
+    print("...KK......KK...**...NN......NN....**.......YY............**")
+    print("...KK......KK...**...NN......NN....**.......YY............**")
+    print()
 
 
 main()
